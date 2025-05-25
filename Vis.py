454ef999e6992f8694af5f981bbe409640b01770
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-05-25 17:19:04 +0530

    Updated Vis.py

--- Vis.py
+++ Vis.py
@@ -9,9 +9,11 @@ plt.ylabel("Number of Reviews")
 plt.show()
 plt.savefig("sentiment_distribution.png")
 # Plot sentiment distribution
+# Plot sentiment distribution
 df["Sentiment"].value_counts().plot(kind="pie", autopct='%1.1f%%', startangle=90, colors=["green", "red", "blue"])
 plt.title("Sentiment Distribution of Flipkart Reviews")
 plt.xlabel("Sentiment")
 plt.ylabel("Number of Reviews")
 plt.show()
 plt.savefig("sentiment_distribution_pie.png")
+
